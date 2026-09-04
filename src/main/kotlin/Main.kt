d1e737a7b8d55eb41d248de81ae43da3c8b75577
Alumno Plaza Vespucio <pv-alumno@duoc.acad>
2026-09-03 20:05:59 -0400

    KOT-002 - Agregar variables y tipos

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,4 +4,23 @@ package org.example
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
     println("GameZone iniciado")
+    val nombreSistema: String = "GameZone"
+
+    val capacidad: Int = 10
+
+    var recaudacionTotal: Double = 0.0
+
+    val codigoConsola: String = "CC12CD"
+    val marca: String = "PlayStation"
+    val modelo: String = "PlayStation 5"
+    val minutosUso: Int = 75
+    val tarifaHora: Double = 800.0
+
+    println("Sistema: $nombreSistema")
+    println("Capacidad: $capacidad")
+    println("Recaudación: $recaudacionTotal")
+
+    recaudacionTotal = 800.0
+
+    println("Nueva recaudación: $recaudacionTotal")
 }
