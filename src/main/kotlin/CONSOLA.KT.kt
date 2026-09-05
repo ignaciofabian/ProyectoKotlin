8341dedabc2396a866a2417126d35e9fea20af88
ignaciofabian <ig.moyah@duocuc.cl>
2026-09-05 10:41:21 -0400

    KOT-005 - Crear clase base Consola

---
+++ src/main/kotlin/CONSOLA.KT.kt
@@ -0,0 +1,8 @@
+package org.example
+
+open class Consola(
+    val codigo: String,
+    val marca: String,
+    val modelo: String,
+    val tipoUsuario: String
+)
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -80,6 +80,23 @@ fun main() {
         println("No tiene beneficio de socio")
     }
 
+    val consola = Consola(
+        codigo = "CC12CD",
+        marca = "Sony",
+        modelo = "PlayStation 5",
+        tipoUsuario = "socio"
+    )
+
+    println(consola.codigo)
+    println(consola.marca)
+    println(consola.modelo)
+    println(consola.tipoUsuario)
+
+
+
+
+
+
 
 
 
